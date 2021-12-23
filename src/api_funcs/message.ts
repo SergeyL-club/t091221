304e6b85d4e34b6d386b5b18ac65c731939099f6
SergeyL-club <damnazzel@gmail.com>
2021-12-23 20:58:03 +0500

    like update

--- src/api_funcs/message.ts
+++ src/api_funcs/message.ts
@@ -85,7 +85,7 @@ const toggleLike = async (account: IAccount, data: inputSetOrRemLike) => {
           },
         }
       );
-      return { Ok: true };
+      return { Ok: true, deleteLike: true };
     } else {
       await Messages.updateOne(
         {
@@ -97,7 +97,7 @@ const toggleLike = async (account: IAccount, data: inputSetOrRemLike) => {
           },
         }
       );
-      return { Ok: true };
+      return { Ok: true, addLike: true };
     }
   }
   return { Ok: false };
