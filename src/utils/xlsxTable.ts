57b0d8d4bae72b12d9b3e641e4853c1997118f04
SergeyL-club <damnazzel@gmail.com>
2022-06-14 17:22:50 +0500

    lvl check

--- src/utils/xlsxTable.ts
+++ src/utils/xlsxTable.ts
@@ -212,7 +212,10 @@ export const importModuleMap = async (
   for (let i = 3; i < worksheet.rowCount; i++) {
     const row = worksheet.getRow(i);
 
-    const lvl = <any>row.getCell(1).value > 1 ? -1 : 0;
+    let lvl = <any>row.getCell(1).value > 1 ? -1 : 0;
+    if (typeof lvl === "undefined") {
+      lvl = -1;
+    }
     const parentKey = <any>row.getCell(2).value?.toString();
     const key = <any>row.getCell(4).value?.toString();
     const name = <any>row.getCell(5).value;
